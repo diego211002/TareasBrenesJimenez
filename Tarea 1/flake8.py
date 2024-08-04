f0ee7e60cf3596c146a1f24962d27fb694f30712
diego211002 <142953161+diego211002@users.noreply.github.com>
2024-08-03 22:59:23 -0600

    Codigo con errores con Flake8
    Cuarto commit de Tarea 1

---
+++ Tarea 1/flake8.py
@@ -0,0 +1,40 @@
+def operation_selector(num1, num2, op):
+    """
+    Selecciona y realiza una operación matemática o bit a bit entre dos números.
+
+    Parámetros:
+    num1 (int): El primer número entero.
+    num2 (int): El segundo número entero.
+    op (str): El operador que define la operación a realizar. Puede ser '+', '-', '*', o '&'.
+
+    """
+    
+    # Verifica qué operación realizar basado en el operador seleccionado
+    if op == '+':
+        return num1 + num2  # Retorna la suma de los dos números
+    elif op == '-':
+        return num1 - num2  # Retorna la resta del primer número menos el segundo
+    elif op == '*':
+        return num1 * num2  # Retorna el producto de los dos números
+    elif op == '&':
+        return num1 & num2  # Retorna la operación bit a bit AND de los dos números
+    else:
+        return "Operación no válida"  # Retorna un mensaje de error si el operador no es reconocido
+
+
+# Solicita al usuario que introduzca el primer número entero
+num1 = int(input("Introduce el primer número entero: "))
+
+# Solicita al usuario que introduzca el segundo número entero
+num2 = int(input("Introduce el segundo número entero: "))
+
+# Solicita al usuario que introduzca el operador deseado
+op = input("Introduce el operador (+, -, *, &): ")
+
+# Realiza la operación seleccionada y almacena el resultado
+resultado = operation_selector(num1, num2, op)
+
+# Muestra el resultado de la operación al usuario
+print(f"El resultado de {num1} {op} {num2} es: {resultado}")
+
+
